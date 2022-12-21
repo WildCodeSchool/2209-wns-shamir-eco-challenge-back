b1b138a6f9f25c5f9755fed5f82efda26e4c31ae
nicovray <auvnic@gmail.com>
2022-12-21 15:51:30 +0100

    correction du bug avec l'ID sur le Type

--- src/inputs/createChallengeInput.ts
+++ src/inputs/createChallengeInput.ts
@@ -3,5 +3,8 @@ import { InputType, Field } from "type-graphql";
 @InputType()
 export class CreateChallengeInput {
   @Field()
+  id!: number;
+
+  @Field()
   name!: string;
 }
--- src/inputs/createGestureInput.ts
+++ src/inputs/createGestureInput.ts
@@ -3,5 +3,8 @@ import { InputType, Field } from "type-graphql";
 @InputType()
 export class CreateGestureInput {
   @Field()
+  id!: number;
+
+  @Field()
   name!: string;
 }
